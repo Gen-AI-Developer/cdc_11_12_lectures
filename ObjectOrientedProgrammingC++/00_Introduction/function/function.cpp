1d8e4c3286255b2186235c193cb65266aad4320c
SyedSafdarAliShah <programmersafdar@live.com>
2025-08-26 05:52:12 +0500

    update

---
+++ ObjectOrientedProgrammingC++/00_Introduction/function/function.cpp
@@ -0,0 +1,31 @@
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     cout << "Enjoy yourself with C++!";
+//     return 0;
+// }
+#include <iostream>
+using namespace std;
+void line(), message();
+// Prototypes
+int main()
+    {
+        cout << "Hello! The program starts in main()."
+        << endl;
+        line();
+        message();
+        line();
+        cout << "At the end of main()." << endl;
+        return 0;
+    }
+void line()
+// To draw a line.
+    {
+        cout << "--------------------------------" << endl;
+    }
+void message()
+// To display a message.
+    {
+        cout << "In function message()." << endl;
+    }
